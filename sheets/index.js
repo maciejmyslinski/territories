c10e841ee014ef24f83a82e90606afa9f9f01c18
Maciej Myśliński <maciej.myslinski@netguru.pl>
2018-05-05 22:52:28 +0200

    Parse territories

--- sheets/index.js
+++ sheets/index.js
@@ -1,3 +1,4 @@
+import isEven from 'is-even';
 import { generateMessages } from './utils/generateMessages';
 
 function getSheetsWithData() {
@@ -16,13 +17,42 @@ function getTerritories(sheets) {
       (_, i) => {
         const row = 2;
         const column = 2 * (i + 1) - 1;
-        const numRows = 53 - row;
-        const numColumns = 1;
+        const numRows = 54 - row;
+        const numColumns = 2;
         return sheet.getRange(row, column, numRows, numColumns).getValues();
       },
     );
-    return [...acc, territories];
+    return [...acc, ...territories];
   }, []);
+}
+
+function parseTerritory(territory) {
+  const number = territory[0][0];
+  const name = territory[1][0];
+  const assignmentsData = territory.slice(2);
+  const assignments = assignmentsData.reduce((acc, row, index, array) => {
+    const isName = isEven(index);
+    if (isName) {
+      return [
+        ...acc,
+        {
+          name: row[0],
+          startDate: array[index + 1][0],
+          endDate: array[index + 1][1],
+        },
+      ];
+    }
+    return acc;
+  }, []);
+  const lastAssignment = assignments.reduce((acc, assignment) => {
+    if (assignment.name) return assignment;
+    return acc;
+  }, undefined);
+  return {
+    number,
+    name,
+    assignment: lastAssignment,
+  };
 }
 
 function doGet() {
@@ -31,5 +61,7 @@ function doGet() {
   ).setMimeType(ContentService.MimeType.JSON);
 }
 function test() {
-  Logger.log(getTerritories(getSheetsWithData()));
+  Logger.log(
+    JSON.stringify(parseTerritory(getTerritories(getSheetsWithData())[0])),
+  );
 }
